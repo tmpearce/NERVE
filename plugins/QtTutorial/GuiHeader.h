e0e5e789d1572dafcc10899fc71a6a4634b3ff45
unknown <Tom@HANGER.(none)>
2012-03-19 10:24:25 -0500

    Signed-off-by: unknown <Tom@HANGER.(none)>

--- plugins/QtTutorial/GuiHeader.h
+++ plugins/QtTutorial/GuiHeader.h
@@ -1,7 +1,6 @@
 #include "ui_QtTutorial.h"
 #include <QIntValidator>
 #include <QString>
-
 class TutorialGui : public QDialog
 {
 	Q_OBJECT//this macro flags the class for the moc tool
@@ -14,6 +13,7 @@ public:
 		ui.val2->setValidator(validator);
 		connect(ui.val1,SIGNAL(textChanged(const QString&)),this,SLOT(val1changed(const QString&)));
 		connect(ui.val2,SIGNAL(textChanged(const QString&)),this,SLOT(val2changed(const QString&)));
+		
 	}
 public slots:
 	void val1changed(const QString& s)
